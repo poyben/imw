161c8fc28619189130f1823e93f58c1bd188738b
ruben <poybenglez@gmail.com>
2019-11-05 10:25:36 +0000

    Final

--- ut2/a3/program1.py
+++ ut2/a3/program1.py
@@ -6,9 +6,9 @@ if num < 0:
     sys.exit('Por favor, introduzca un valor positivo.')
 
 else:
-    for value in range(2, num + 1):
-        if value % num == 0:
-            print ('Es primo!')
+    for value in range(2, num):
+        if num % value == 0:
+            print ('No es primo!')
             break
     else:
-        print ('No es primo!')
+        print ('Es primo!')
--- ut2/a3/program4.py
+++ ut2/a3/program4.py
@@ -6,7 +6,7 @@ if num < 0:
     sys.exit('Por favor, escriba dos valores positivos.')
 else:
     for i in range(1, num + 1):
-        j = 1
+        result = 1
         for k in range (i, 0, -1):
-            j *= k
-        print (i, '!=', j)
+            result *= k
+        print (i,'! =', result)
